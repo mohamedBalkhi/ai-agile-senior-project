d7c011e083c70a70e755d8c6452d3e746d2eecb6
Mohamed Al Balkhi <mohamedx2754@gmail.com>
2024-10-18 20:00:42 +0300

    Added the dependency of MediaTR

---
+++ Senior.AgileAI.BaseMgt.Application/ApplicationDependencyContainer.cs
@@ -0,0 +1,19 @@
+
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Senior.AgileAI.BaseMgt.Infrastructure;
+
+public static class InfrastructureDependencyContainer
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+
+        // Add MediaTr
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        
+
+        return services;
+    }
+}
